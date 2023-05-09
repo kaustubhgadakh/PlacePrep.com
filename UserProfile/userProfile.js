14f4b83bcdf43e9317d69d53c43b34a84aa4b448
kaustubhgadakh <kaustubhgadakh2001@gmail.com>
2023-05-09 12:58:09 +0530

    added project

---
+++ UserProfile/userProfile.js
@@ -0,0 +1,170 @@
+
+var data=JSON.parse(localStorage.getItem("user"));
+console.log(data.password);
+
+document.getElementById("logout").addEventListener("click",()=>{
+    localStorage.clear();
+    alert("Logged Out Sucessfully!");
+    window.location.href="../index.html"
+})
+
+document.getElementById("sdetail").addEventListener("click",()=>{
+    location.href="../StudentDetails/student.html"
+})
+
+document.querySelector("form").addEventListener("submit", fun);
+var nameField = document.getElementById("na");
+var mail = document.getElementById("email");
+var number = document.getElementById("mobile");
+var college = document.getElementById("college");
+var city = document.getElementById("city");
+var github = document.getElementById("gi");
+var geeks = document.getElementById("gfg");
+var chef = document.getElementById("codechef");
+var hacker = document.getElementById("hacker");
+var score = document.getElementById("score");
+
+document.getElementById("mcq").addEventListener("click",()=>{
+    window.open("../data/index.html")
+})
+
+window.onload = function () {
+    loadUser();
+    console.log(mail.value)
+    if((mail.value=="kaustubhgadakh@gmail.com" && data.password=="admin")||(mail.value=="sonvanesaurabh77@gmail.com" && data.password=="admin")){
+            document.getElementById("mcq").style.display="block";
+            document.getElementById("sdetail").style.display="block";
+            document.getElementById("prof").textContent="Admin";
+    }
+    let joinNowBtn = document.querySelector(".join-us-btn");
+    let user = JSON.parse(localStorage.getItem('user'));
+
+    if(user){
+        document.getElementById("logout").style.display="block"
+    }
+
+    let loc = "../login/loginSignup.html";
+    console.log('xxx', user);
+    if (user) {
+        loc = "../UserProfile/userProfile.html";
+        joinNowBtn.textContent = 'Profile'
+    }
+    joinNowBtn.addEventListener('click', function () {
+        location.href = loc;
+    })
+};
+
+
+
+function loadUser() {
+    let user = JSON.parse(localStorage.getItem('user'))
+    nameField.value = user?.name || "";
+    mail.value = user?.email || "";
+    number.value = user?.mobile || "";
+    score.textContent = user?.totalScore;
+    college.value = user?.college || "";
+    city.value = user?.city || "";
+    github.value = user?.github || "";
+    geeks.value = user?.gfg || "";
+    chef.value = user?.codeChef || "";
+    hacker.value = user?.hackerRank || "";
+    document.getElementById("name").innerText = user?.name || "";
+    document.getElementById("coll").innerText = user?.college || "";
+    document.getElementById("cit").innerText = user?.city || "";
+    document.getElementById("github").href = user?.github || "";
+    document.getElementById("geeksforgeeks").href = user?.gfg || "";
+    document.getElementById("codef").href = user?.codeChef || "";
+    document.getElementById("HackerRank").href = user?.hackerRank || "";
+    document.getElementById("Gmail").innerText = user?.email;
+}
+
+
+
+function fun(event) {
+    event.preventDefault()
+    nameFieldValue = nameField.value;
+    mailValue = mail.value;
+    NumberValue = number.value;
+    collegeValue = college.value;
+    cityValue = city.value;
+    githubValue = github.value;
+    geeksValue = geeks.value;
+    chefValue = chef.value;
+    hackerValue = hacker.value;
+
+
+
+    console.log(nameFieldValue, mailValue, NumberValue, collegeValue, cityValue, githubValue, geeksValue, chefValue, hackerValue);
+    let id = JSON.parse(localStorage.getItem('user')).id;
+    console.log("id: ", id);
+    let body = JSON.stringify({
+        name: nameFieldValue,
+        college: collegeValue,
+        email: mailValue,
+        mobile: NumberValue,
+        city: cityValue,
+        github: githubValue,
+        hackerRank: hackerValue,
+        codeChef: chefValue,
+        gfg: geeksValue
+    })
+    updateUser({ body: body, id: id });
+    // document.getElementById("na").value = "";
+    // document.getElementById("email").value = "";
+    // document.getElementById("mobile").value = "";
+    // document.getElementById("college").value = "";
+    // document.getElementById("city").value = "";
+    // document.getElementById("gi").value = "";
+    // document.getElementById("gfg").value = "";
+    // document.getElementById("codechef").value = "";
+    // document.getElementById("hacker").value = "";
+}
+async function updateUser({ body, id }) {
+    console.log(body);
+    try {
+        const res = await fetch(`https://placeprepbackend.onrender.com/users/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: body,
+        });
+        console.log("updateUser res", res);
+        getUser(id);
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+async function getUser(id) {
+    try {
+        const res = await fetch(`https://placeprepbackend.onrender.com/users/${id}`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let json = await res.json();
+        console.log("getUser json", json);
+        if (json) {
+            localStorage.setItem("user", JSON.stringify(json));
+            loadUser();
+        }
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+async function getScores() {
+    let user = JSON.parse(localStorage.getItem('user'));
+    try {
+        const res = await fetch(`https://placeprepbackend.onrender.com/users`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let json = await res.json();
+        return json;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
---
+++ login/loginSignup.js
@@ -0,0 +1,146 @@
+console.clear();
+
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+const loginSubmitBtn = document.getElementById('login-btn');
+const signupSubmitBtn = document.getElementById('signup-btn');
+const optBtn = document.getElementById('otp-continue');
+const optCloseBtn = document.getElementById('otp-close-btn');
+const forgotPasswordBtn = document.getElementById('forgotPass');
+
+window.onload = () => {
+    emailjs.init("hcdI0IYnnpZH1LOX2");
+}
+forgotPasswordBtn.addEventListener('click', async () => {
+    let email = document.getElementById('login-email').value;
+    let user = await getUserWithEmail({ email: email });
+    console.log(user);
+    if (user) {
+        emailjs.send("service_t7pt8z5", "template_vg8rq5r", { from_name: 'PlacePrep', to_name: user.name, to_email: email, the_pass: user.password, })
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Your Password has been sent to your registered email')
+            }, function (error) {
+                alert('An error has occurred while sending your password')
+                console.log('FAILED...', error);
+            });
+    } else {
+        alert('Email not found! Please try again')
+    }
+})
+loginBtn.addEventListener('click', (e) => {
+    let parent = e.target.parentNode.parentNode;
+    Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+        if (element !== "slide-up") {
+            parent.classList.add('slide-up')
+        } else {
+            signupBtn.parentNode.classList.add('slide-up')
+            parent.classList.remove('slide-up')
+        }
+    });
+});
+
+signupBtn.addEventListener('click', (e) => {
+    let parent = e.target.parentNode;
+    Array.from(e.target.parentNode.classList).find((element) => {
+        if (element !== "slide-up") {
+            parent.classList.add('slide-up')
+        } else {
+            loginBtn.parentNode.parentNode.classList.add('slide-up')
+            parent.classList.remove('slide-up')
+        }
+    });
+});
+
+signupSubmitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let name = document.getElementById('user-name').value;
+    let email = document.getElementById('user-email').value;
+    let otpToSend = Math.floor(1000 + Math.random() * 9000);
+    localStorage.setItem('otp', otpToSend);
+    emailjs.send("service_t7pt8z5", "template_5e59gpr", { from_name: 'PlacePrep', to_name: name, to_email: email, the_otp: otpToSend, reply_to: email })
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
+    $('#otpModal').modal({ show: true, backdrop: 'static' })
+})
+loginSubmitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let email = document.getElementById('login-email').value;
+    let password = document.getElementById('login-password').value;
+    console.log(email, password);
+    getUser({ email, password })
+})
+optBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let name = document.getElementById('user-name').value;
+    let email = document.getElementById('user-email').value;
+    let password = document.getElementById('user-password').value;
+    console.log(name, email, password);
+    let otpInput = document.getElementById('otp-input').value;
+    let otp = localStorage.getItem('otp');
+    console.log(otp, otpInput, otp == otpInput);
+    if (otp == otpInput) {
+        verifyOtp(name, email, password)
+    }
+})
+
+function verifyOtp(name, email, password) {
+
+    addUser({ body: JSON.stringify({ name: name, email: email, password: password, totalScore: 0, quizScores: {} }) })
+    optCloseBtn.click();
+    loginBtn.click();
+}
+
+async function addUser({ body }) {
+    console.log(body);
+    try {
+        const res = await fetch(`https://placeprepbackend.onrender.com/users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: body,
+        });
+        console.log("res", res);
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+async function getUser({ email, password }) {
+    try {
+        const res = await fetch(`https://placeprepbackend.onrender.com/users?email=${email}&password=${password}`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let json = await res.json();
+        console.log(json, 'sss');
+        if (json.length > 0) {
+            localStorage.setItem("user", JSON.stringify(json[0]));
+            location.href = '../HomePage/index.html'
+        }
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+async function getUserWithEmail({ email }) {
+    try {
+        const res = await fetch(`https://placeprepbackend.onrender.com/users?email=${email}`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let json = await res.json();
+        console.log(json, 'sss');
+        if (json.length > 0) {
+            return json[0];
+        }
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
